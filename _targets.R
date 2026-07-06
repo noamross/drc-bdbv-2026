ff5bd301887e0ed62798c76921fe81c2cefe8f8f
Noam Ross <noam.ross@gmail.com>
2026-07-05 21:02:02 -0400

    Use local consistent=TRUE for targets CAS

--- _targets.R
+++ _targets.R
@@ -4,10 +4,11 @@ library(tarchetypes)
 tar_option_set(
   controller = crew::crew_controller_local(
     name = "primary",
-    workers = as.integer(Sys.getenv("NPROC", unset = "12"))
+    workers = as.integer(Sys.getenv("NPROC", unset = "12")),
+    local_log_directory = "crew_logs"
   ),
   format = "qs",
-  repository = tar_repository_cas_local(),
+  repository = tar_repository_cas_local(consistent = TRUE),
   packages = c("dplyr", "tidyr", "purrr", "stringi", "tibble")
 )
 
